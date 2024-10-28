01fabad257980dd7c5e519457d4bbe4b87dba131
QISUN000 <qsun4@rrc.ca>
2024-10-27 22:27:35 -0500

    1

--- src/main/java/com/example/nail_salon_booking_backend/NailSalonBookingBackendApplication.java
+++ src/main/java/com/example/nail_salon_booking_backend/NailSalonBookingBackendApplication.java
@@ -2,16 +2,21 @@ package com.example.nail_salon_booking_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import com.example.nail_salon_booking_backend.security.SecurityConfig;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(SecurityConfig.class)
+public class NailSalonBookingBackendApplication extends SpringBootServletInitializer {
 
-public class NailSalonBookingBackendApplication {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NailSalonBookingBackendApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NailSalonBookingBackendApplication.class, args);
 	}
-
 }
